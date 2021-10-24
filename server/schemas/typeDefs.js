7519a36853918b0bf92a0794679f571c71f63345
Arne Tripolone <arnetripolone@gmail.com>
2021-10-24 18:52:44 +1100

    Added graphQL Book query to typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,14 @@
+const {gql} =require('apollo-server-express');
+
+const typeDefs = gql`
+    type Book{
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+`;
+
+module.exports = typeDefs;
