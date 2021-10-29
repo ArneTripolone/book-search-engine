6a27cd96f40804215fd906d31cb8df15fcd43609
Arne Tripolone <arnetripolone@gmail.com>
2021-10-29 22:16:40 +1100

    getting ready for heroku

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import 'express-async-errors'
 
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
